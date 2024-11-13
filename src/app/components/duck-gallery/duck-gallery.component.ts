71790702ee747ee8876856939ecfae4bd6572395
OsmanyCruz <osmanycruzgarcia@gmail.com>
2024-11-13 17:59:29 -0300

    boostrrap modules

--- src/app/components/duck-gallery/duck-gallery.component.ts
+++ src/app/components/duck-gallery/duck-gallery.component.ts
@@ -2,14 +2,16 @@
 import { Component } from '@angular/core';
 import { DuckService } from '../../services/duck.service';
 import { CommonModule } from '@angular/common'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-duck-gallery',
   standalone: true, 
   templateUrl: './duck-gallery.component.html',
-  styleUrls: ['./duck-gallery.component.css'],
-  imports: [CommonModule] 
+  styleUrls: ['./duck-gallery.component.scss'],
+  imports: [CommonModule,MatButtonModule,MatInputModule] 
 })
 export class DuckGalleryComponent {
   imageUrls: string[] = [];
